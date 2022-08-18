8255cceecf0854487c0bcf1453508d0c027c03b1
Pedro Wasc <101606453+pedrowasc@users.noreply.github.com>
2022-08-17 21:56:00 -0300

    terraform update

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,10 +1,14 @@
 provider "aws" {
   version = "~>2.0"
-  region = "us-east-1"
+  region  = "us-east-1"
 }
 
 resource "aws_instance" "Desenvolvimento" {
-    ami = "ubuntuXXXXXX" 
-    instance_instance_type = "t2.micro" 
-    key_name = ""
+  count                  = 3
+  ami                    = "ami-08e09cb6129479a29"
+  instance_instance_type = "t2.micro"
+  key_name               = "terraform-aws"
+  tags = {
+    Name = "Desenvolvimento${count.index}"
+  }
 }
